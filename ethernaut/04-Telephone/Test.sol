97895dee81ff08b70fa55c3926410f108ca5a040
man715 <man715@webhack.localdomain>
2022-05-05 15:41:31 -0600

    vault backup: 2022-05-05 15:41:31

--- ethernaut/04-Telephone/Test.sol
+++ ethernaut/04-Telephone/Test.sol
@@ -5,27 +5,24 @@ import './Telephone.sol';
 
 contract RingRing {
   Test phone = Test(0x4242);
-
-  constructor() public payable {
-      phone.changeOwner(address(0x20000));
-  }
-
+  
   function pwn() public {
-    phone.changeOwner(address(0x30000));
+    phone.changeOwner(address(0x10000));
   }
 }
 
 contract Test is Telephone {
+    RingRing ring;
   constructor() Telephone() public payable {
     // hint echidna
-    new RingRing();
+    ring = new RingRing();
   }
 
-  function echidna_test_owner() public returns (bool) {
-    if (owner == msg.sender) {
-        return true;
-    } else {
-        return true;
-    }
-  }
+    function echidna_test_origin() public returns (bool) {
+        if (tx.origin == owner) {
+            return false;
+        } else {
+            return true;
+        }
+    }    
 }
