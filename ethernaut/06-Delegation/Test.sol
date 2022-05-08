62f718f5ad1e88c4115ea627a371a96b3b60a455
man715 <man715@webhack.localdomain>
2022-05-07 19:41:31 -0600

    vault backup: 2022-05-07 19:41:31

--- ethernaut/06-Delegation/Test.sol
+++ ethernaut/06-Delegation/Test.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.6.0;
 
 import 'Delegation.sol';
 
-contract Test is Delegation {
+interface Target {
+    function pwn() external;
+}
+
+contract Test is Delegation, Target {
   constructor() Delegation(address(new Delegate(msg.sender))) public { }
 
-  function pwn2own() public {
-    // this should trigger our fallback fn
-    address(this).call(abi.encodeWithSignature("pwn()"));
-  }
+//   function pwn2own() public {
+//     // this should trigger our fallback fn
+//     address(this).call(abi.encodeWithSignature("pwn()"));
+//   }
 
   function echidna_test_owner() public returns (bool) {
     if (owner == address(0x90000)) {
