8b4d38b4557cabe18f1d714e710f879d1f0a1a40
Man715 <man715@protonmail.com>
2022-06-02 14:40:41 -0600

    vault backup: 2022-06-02 14:40:41

--- ethernaut/21-Dex/Test.sol
+++ ethernaut/21-Dex/Test.sol
@@ -3,18 +3,26 @@ pragma solidity ^0.6.0;
 import "Dex.sol";
 
 contract Test is Dex {
-    SwappableToken token1;
-    SwappableToken token2;
 
-    constructor() Dex() {
-        token1 = new SwappableToken(address(this), "Token1", "t1", 90 );
-        token2 = new SwappableToken(address(this), "Token2", "t2", 90 );
-        token1.mint(address(0x10000), 10);
-        token2.mint(address(0x10000), 10);
+    constructor() Dex() public {
+        SwappableToken _token1 = new SwappableToken(address(this), "Token1", "t1", 100 );
+        SwappableToken _token2 = new SwappableToken(address(this), "Token2", "t2", 100 );
+        _token1.transfer(address(0x10000), 10);
+        _token2.transfer(address(0x10000), 10);
+        token1 = address(_token1);
+        token2 = address(_token2);
     }
 
-    function echidna_test_dex_balance() public returns (bool) {
-        return token1.balanceOf(address(this)) > 1 && token2.balanceOf(address(this)) > 1;
+    function echidna_test_dex_balance_token1() public returns (bool) {
+        return (SwappableToken(token1).balanceOf(address(this)) > 1);
+    }
+
+    function echidna_test_dex_balance_token2() public returns (bool) {
+        return (SwappableToken(token2).balanceOf(address(this)) > 1);
+    }
+
+    function echidna_test_attacker_balance_token1() public returns (bool) {
+        return (SwappableToken(token1).balanceOf(address(0x10000)) > 1);
     }
 
 }
